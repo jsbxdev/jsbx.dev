40dfabdddcfe4439783fc058f9129896eac11846
Eduardo Barros <joseeduardobarros@gmail.com>
2020-03-19 23:56:24 -0300

    Add Header and Logo

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header
+    class="header flex justify-between items-center h-20 p-5 mt-0 w-full max-w-screen-lg bg-white"
+  >
+    <Logo></Logo>
+    <nav class="">
+      <g-link class="nav__link" to="/about/">sobre</g-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
+
+<style>
+.header {
+  border-bottom: solid 1px #dddddd;
+}
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 class="logo">
+    <g-link to="/">JSB<span class="x">X</span></g-link>
+  </h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.logo {
+  font-family: "Montserrat", sans-serif;
+  font-weight: 900;
+  font-size: 1.3125rem;
+}
+
+.logo.x {
+  color: red;
+}
+</style>
